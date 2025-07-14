948b642d796eaabb682d2563f295b61ab20f7822
Jesse Shaw <JesseShawCodes@gmail.com>
2025-07-13 21:45:08 -0400

    Add GitHub link to Footer component

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,6 +3,7 @@
 import { React } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
+  faGithub,
   faInstagram, faLinkedin, faXTwitter,
 } from '@fortawesome/free-brands-svg-icons';
 
@@ -12,10 +13,12 @@ function Footer() {
       <footer className="container py-3 my-4" role="contentinfo">
         <div className="d-flex align-items-center justify-content-between w-100">
           <div className="mb-3 mb-md-0">© 2025 {process.env.NEXT_PUBLIC_NAME}</div>
-          <ul className="nav col-md-4 list-unstyled">
+          <ul className="nav list-unstyled">
             <li className="ms-3"><a className="text-body-secondary" href="https://www.instagram.com/jesseshawcodes/" title="Instagram" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a></li>
             <li className="ms-3"><a className="text-body-secondary" href="https://www.linkedin.com/in/itsjesseshaw/" title="LinkedIn" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a></li>
             <li className="ms-3"><a className="text-body-secondary" href="https://x.com/jesseshawcodes" title="Twitter" target="_blank"><FontAwesomeIcon icon={faXTwitter} /></a></li>
+            <li className="ms-3"><a className="text-body-secondary" href="http://github.com/jesseShawCodes" title="GitHub" target="_blank"><FontAwesomeIcon icon={faGithub} /></a></li>
+
           </ul>
         </div>
       </footer>
