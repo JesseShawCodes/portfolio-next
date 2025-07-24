e0002ac5a3fadf20afa5be084f3b324b119249d0
Jesse Shaw <JesseShawCodes@gmail.com>
2025-07-24 14:11:23 -0400

    Updating project links, logo size

--- src/app/portfolio/projects.jsx
+++ src/app/portfolio/projects.jsx
@@ -1,19 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Project({project}) {
-  console.log(project);
   return (
-    <div className="project card">
+    <div className="project card my-4">
       <div className="card-header">
       <h1>{project.name}</h1>
-      <div><Image src={`${project.logo.url}`} width={30} height={40} alt={`${project.name} logo`} /></div>
+      <div><Image src={`${project.logo.url}`} width={80} height={80} alt={`${project.name} logo`} /></div>
       </div>
       <div className="card-body">
         <div>
           {project.description}
         </div>
         <div>
+          <Link href={project.link} target="_blank">
           {project.link}
+          </Link>
         </div>
       </div>
     </div>
