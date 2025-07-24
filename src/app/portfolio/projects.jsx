ef8878118aab5ac35e50850f5f1609baad2712e7
Jesse Shaw <JesseShawCodes@gmail.com>
2025-07-24 14:00:31 -0400

    Corrected image source error for projects page

--- src/app/portfolio/projects.jsx
+++ src/app/portfolio/projects.jsx
@@ -9,7 +9,12 @@ function Project({project}) {
       <div><Image src={`${project.logo.url}`} width={30} height={40} alt={`${project.name} logo`} /></div>
       </div>
       <div className="card-body">
-        {project.description}
+        <div>
+          {project.description}
+        </div>
+        <div>
+          {project.link}
+        </div>
       </div>
     </div>
   );
