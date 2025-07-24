c55c2a731518a14b12d913856fbab95cb4a0da9c
Jesse Shaw <JesseShawCodes@gmail.com>
2025-07-24 13:25:38 -0400

    feat: add Project component and fetch projects data in Page component

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -4,10 +4,12 @@ import P5Sketch from "./sketch";
 import Repo from "../components/Repo";
 import { Octokit } from "octokit";
 import Link from "next/link";
+import Project from "./projects";
 
 export default function Page() {
   const p5Ref = useRef(null);
   const [repositories, setRepositories] = useState([]);
+  const [projects, setProjects] = useState([]);
 
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
@@ -39,6 +41,27 @@ export default function Page() {
     })();
   }, []);
 
+  useEffect(() => {
+    (async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/projects?populate=logo`, {
+          cache: 'force-cache',
+        });
+
+        const data = await res.json();
+        setProjects(data.data);
+      } catch (error) {
+        setIsError({
+          error: true,
+          message: error,
+        });
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
   if (isLoading) {
     return <div className="container">Loading...</div>;
   }
@@ -56,9 +79,27 @@ export default function Page() {
       <div>
         <p>My time is currently being taken up by my Full Time job at Amentum. But I do wanna make sure I am putting in extra time to keep up with new tools and work on some of my own projects to stay sharp.</p>
 
-        <p>One passion project I am working on right now is a music rating application built using React, Django, and a MySQL database. The Git Repository for that project can be found below (Music March Madness). This application can be tested here: <Link href="https://dadgad.netlify.app/" target="_blank">Dadgad Live Application</Link></p>
+          <div>
+            {
+              isError
+                ? (
+                  <div>
+                    <p>
+                      {isError.message.message}
+                    </p>
+                  </div>
+                )
+                : (
+                  projects.map(
+                    (project, index) => <Project project={project} key={index} />,
+                  )
+                )
+            }
+          </div>
 
-        <p>If you would like to keep up with all of the other work I am doing outside of my work at Amentum, please check out my GitHub projects below</p>
+        <p>If you would like to keep up with all of the other work I am doing outside of my work at Amentum, please check out my GitHub projects below or <Link href="https://github.com/JesseShawCodes" target='_blank'>
+            visit my GitHub profile
+        </Link>.</p>
         <div style={{ maxWidth: '1200px', margin: '0px auto' }}>
           <div>
             {
---
+++ src/app/portfolio/projects.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+function Project({project}) {
+  console.log(project);
+  return (
+    <div className="project card">
+      <div className="card-header">
+      <h1>{project.name}</h1>
+      <div><Image src={`https://${process.env.NEXT_PUBLIC_API_ROOT_MEDIA}${project.logo.url}`} width={30} height={40} alt={`${project.name} logo`} /></div>
+      </div>
+      <div className="card-body">
+        {project.description}
+      </div>
+    </div>
+  );
+}
+
+export default Project;
