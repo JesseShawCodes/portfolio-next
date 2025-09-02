87766faa4fdfc55e590565f9655539bc709cfc83
Jesse Shaw <JesseShawCodes@gmail.com>
2025-09-02 09:45:06 -0400

    remove unused mock server and instrumentation code

--- src/instrumentation.js
+++
@@ -1,7 +0,0 @@
-// instrumentation.js
-export async function register() {
-  if (process.env.NODE_ENV === 'development') {
-    const { server } = require('../mocks/server');
-    server.listen();
-  }
-}
