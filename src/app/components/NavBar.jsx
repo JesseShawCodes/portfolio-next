ca3b1d0bd3a0e584591b6e89b743697a4ecde107
Jesse Shaw <JesseShawCodes@gmail.com>
2025-09-05 12:21:20 -0400

    Add floating controls for back-to-top and theme toggle functionality with responsive design adjustments

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,7 +1,6 @@
 "use client"
 import { React } from 'react';
 import Link from 'next/link';
-import ThemeToggleButton from './ThemeToggleButton';
 import NavLink from './NavLink';
 import Image from 'next/image';
 
