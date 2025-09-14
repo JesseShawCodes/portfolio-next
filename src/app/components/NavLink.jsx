022dba9ad799324e85ce4ca57fd95b3159c52a26
Jesse Shaw <JesseShawCodes@gmail.com>
2025-09-13 22:16:36 -0400

    NavLink renders active class only for isActive page

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,10 +1,14 @@
+'use client';
 import { React } from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 function NavLink({ name, link }) {
+  const pathname = usePathname();
+  const isActive = pathname === link;
   return (
     <li className="nav-item">
-      <Link href={link} className="nav-link active">{name}</Link>
+      <Link href={link} className={`nav-link ${isActive ? 'active' : ''}`}>{name}</Link>
     </li>
   );
 }
